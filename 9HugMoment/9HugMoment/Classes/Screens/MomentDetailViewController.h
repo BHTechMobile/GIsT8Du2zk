f57e0dabde11c9f242e8c9a7e88b8cfeb805462f
Nong Trung Nghia <nghianong.bhtech@gmail.com>
2014-11-24 18:17:25 +0700

    init

---
+++ 9HugMoment/9HugMoment/Classes/Screens/MomentDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MomentDetailViewController.h
+//  9HugMoment
+//
+//  Created by Tommy on 11/24/14.
+//  Copyright (c) 2014 BHTech Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MomentDetailViewController : UIViewController
+
+@end
