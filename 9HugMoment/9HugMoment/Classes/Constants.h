150b3925e1eab7e507feeee78edcbfedce49f30b
phuongphbhtech <phuongpham.bhtech@gmail.com>
2014-11-27 10:05:06 +0700

    Add_File_Constants_And_Prefix_PCH

---
+++ 9HugMoment/9HugMoment/Classes/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  9HugMoment
+//
+//  Created by PhamHuuPhuong on 27/11/14.
+//  Copyright (c) 2014 BHTech Mobile. All rights reserved.
+//
+
+#ifndef _HugMoment_Constants_h
+#define _HugMoment_Constants_h
+
+
+#endif
