000abe61be48cdae6d5c19f1d91dc544a6df7381
phuongphbhtech <phuongpham.bhtech@gmail.com>
2014-12-06 15:25:39 +0700

    Implement_UI

--- 9HugMoment/9HugMoment/Classes/Screens/MomentDetailViewController.m
+++ 9HugMoment/9HugMoment/Classes/Screens/MomentDetailViewController.m
@@ -16,22 +16,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [_contentScrollView addSubview:_playerView];
+    CGRect frameExtend = [_extendView frame];
+    frameExtend.origin.y = CGRectGetHeight(_playerView.frame);
+    [_extendView setFrame:frameExtend];
+    CGRect frameScroll = [_contentScrollView frame];
+    frameScroll.size.height= CGRectGetHeight(_playerView.frame)+CGRectGetHeight(_extendView.frame);
+    [_contentScrollView setFrame:frameScroll];
+    [_contentScrollView addSubview:_extendView];
+    [_contentScrollView setContentSize:CGSizeMake(320, CGRectGetHeight(_contentScrollView.frame)+ (IS_IPHONE_5 ? 330:400))];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - Button Control
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)momentSegmentControllerAction:(id)sender {
+    
 }
-*/
 
+- (IBAction)addVideoButtonAction:(id)sender {
+    
+}
+
+- (IBAction)backButtonAction:(id)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
 @end
