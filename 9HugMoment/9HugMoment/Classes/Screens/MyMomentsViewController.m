a8093179416a562ff308fee87993bce7a9706215
phuongphbhtech <phuongpham.bhtech@gmail.com>
2014-12-15 17:29:32 +0700

    Update_UI_Model_Service_In_My_Moments_Page

--- 9HugMoment/9HugMoment/Classes/Screens/MyMomentsViewController.m
+++ 9HugMoment/9HugMoment/Classes/Screens/MyMomentsViewController.m
@@ -7,33 +7,224 @@
 //
 
 #import "MyMomentsViewController.h"
+#import "CaptureVideoViewController.h"
+#import "FBConnectViewController.h"
+#import "MomentsModel.h"
+#import "MomentDetailViewController.h"
+#import "ODRefreshControl.h"
+#import "MyMomentsModel.h"
+#import "ArrayDataSource.h"
+#define kYouTitle @"My Moments"
+
+static NSString * const MomentViewCellIdentifier = @"MomentViewCellIdentifier";
 
 @interface MyMomentsViewController ()
 
-@end
-#define kYouTitle @"My Moments"
+@property (nonatomic, strong) FacebookManager *_facebookManager;
+@property (nonatomic, strong) ArrayDataSource *arrayDataSource;
 
-@implementation MyMomentsViewController
+@end
+
+@implementation MyMomentsViewController{
+    CaptureVideoViewController *captureVideoViewController;
+    FBConnectViewController *fbConnectViewController;
+    MomentsModel *_momentModel;
+    MyMomentsModel *_myMomentModel;
+    DownloadVideoView *_downloadVideoView;
+    MomentDetailViewController *momentDetailViewController;
+    MessageObject *message;
+    ODRefreshControl *_refreshControl;
+    MBProgressHUD *_hud;
+    NSCache *_avatarCache;
+}
+@synthesize _facebookManager;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = kYouTitle;
-    // Do any additional setup after loading the view.
+    _momentModel = [[MomentsModel alloc] init];
+    _avatarCache = [[NSCache alloc] init];
+    _downloadVideoView = [DownloadVideoView fromNib];
+    CGRect downloadVideoFrame = self.view.frame;
+    downloadVideoFrame.origin.y = self.myMessagesTableView.frame.origin.y;
+    downloadVideoFrame.size.height = downloadVideoFrame.size.height - self.myMessagesTableView.frame.origin.y;
+    _downloadVideoView.frame = downloadVideoFrame;
+    _downloadVideoView.alpha = 0.0;
+    _downloadVideoView.delegate = self;
+    [self.view addSubview:_downloadVideoView];
+    
+    _hud = [[MBProgressHUD alloc] initWithView:self.view];
+    _hud.userInteractionEnabled = NO;
+    _hud.labelText = NSLocalizedString(@"Loading...", nil);
+    _hud.labelFont = [UIFont fontWithName:@"HelveticaNeue" size:14.0];
+    _hud.mode = MBProgressHUDModeIndeterminate;
+    [self.view addSubview:_hud];
+    
+    _refreshControl = [[ODRefreshControl alloc] initInScrollView:self.myMessagesTableView];
+    [_refreshControl addTarget:self action:@selector(refreshTableView) forControlEvents:UIControlEventValueChanged];
+
+    CGRect frameExtend2 = [_newsMomentButton frame];
+    frameExtend2.origin.y = WIDTH_BUTTON_NEW_MOMENTS;
+    frameExtend2.origin.x = CGRectGetWidth(self.view.frame)/3;
+    frameExtend2.size.width = CGRectGetWidth(self.view.frame)/3;
+    frameExtend2.size.height = HIGHT_BUTTON_NEW_MOMENTS;
+    
+    _newsMomentButton = [[UIButton alloc] initWithFrame:frameExtend2];
+    [_newsMomentButton setBackgroundColor:[UIColor lightGrayColor]];
+    _newsMomentButton.layer.cornerRadius = CORNER_RADIUS;
+    
+    [_newsMomentButton setTitle:TITLE_BUTTON_NEW_MOMENTS forState:UIControlStateNormal];
+    [_newsMomentButton.titleLabel setFont:[UIFont systemFontOfSize:TITLE_BUTTON_NEW_MOMENTS_FONT_SIZE]];
+    [_newsMomentButton addTarget:self action:@selector(callPullDownRequest:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_newsMomentButton];
+    
+    self.newsMomentButton.hidden = YES;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(pushNSNotifications:)
+                                                 name:CALL_PUSH_NOTIFICATIONS object:nil];
+//    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:[sender tag] inSection:0];
+//    [self setupTableView:indexPath];
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+- (void)pushNSNotifications:(NSNotification*)notify{
+    self.newsMomentButton.hidden = NO;
+}
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
+#pragma mark - Custom Methods
+
+- (void)getAllMessage {
+    _newsMomentButton.hidden = YES;
+    [_hud show:YES];
+    [_momentModel getAllMessagesSuccess:^(id result) {
+        NSLog(@"count : %lu",(unsigned long)_myMomentModel.messages.count);
+        [_myMessagesTableView reloadData];
+        [_hud hide:YES];
+    } failure:^(NSError *error) {
+        [_hud hide:YES];
+    }];
+}
+
+#pragma mark - Segue
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([[segue identifier] isEqualToString:@"pushMomentDetailsView"]) {
+        momentDetailViewController = [segue destinationViewController];
+        momentDetailViewController.capturePath = [NSURL fileURLWithPath:message.localVideoPath];
+        momentDetailViewController.messageObject = message;
+        momentDetailViewController.hidesBottomBarWhenPushed = YES;
+        NSLog(@"message.localVideoPath %@",message.localVideoPath);
+        NSLog(@"momentDetailViewController.capture %@",momentDetailViewController.capturePath);
+    }
 }
-*/
+
+#pragma mark - TableView delegates & datasources
+
+- (void)setupTableView:(NSIndexPath *)indexPath{
+    TableViewCellConfigureBlock configureCell = ^(MomentsMessageTableViewCell *momentsMessageTableViewCell,MyMomentsModel *myMomentsModel){
+        MessageObject *mymessage = (MessageObject *)[_myMomentModel.messages objectAtIndex:indexPath.row];
+        [momentsMessageTableViewCell setMessageWithMessage:mymessage];
+    };
+    NSArray *myMessage = _myMomentModel.messages;
+    _arrayDataSource = [[ArrayDataSource alloc]initWithItems:myMessage cellIdentifier:MomentViewCellIdentifier configureCellBlock:configureCell];
+    self.myMessagesTableView.dataSource = self.arrayDataSource;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return NUMBER_OF_SECTION_TABLE_VIEW_MOMENTS_VIEW_CONTROLLER;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return HEIGHT_ROW_TABLE_VIEW_MOMENTS_VIEW_CONTROLLER;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _myMomentModel.messages.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    MomentsMessageTableViewCell *cell = (MomentsMessageTableViewCell *)[tableView dequeueReusableCellWithIdentifier:IDENTIFIER_MOMENTS_MESSAGE_TABLE_VIEW_CELL];
+    if (!cell) {
+        cell = [[MomentsMessageTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:IDENTIFIER_MOMENTS_MESSAGE_TABLE_VIEW_CELL];
+    }
+    MessageObject *mymessage = (MessageObject *)[_myMomentModel.messages objectAtIndex:indexPath.row];
+    [cell setMessageWithMessage:mymessage];
+    UIImage *userAvatar = [_avatarCache objectForKey:mymessage.userFacebookID];
+    if (!userAvatar) {
+        [BaseServices downloadUserImageWithFacebookID:mymessage.userFacebookID success:^(AFHTTPRequestOperation *operation, id responseObject){
+            cell.thumbnailImageView.image = responseObject;
+            [_avatarCache setObject:responseObject forKey:mymessage.userFacebookID];
+        }failure:^(AFHTTPRequestOperation *operation, NSError *error){
+            NSLog(@"Error get user image from facebook: %@",error);
+        }];
+    }else {
+        cell.thumbnailImageView.image = userAvatar;
+    }
+    [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    message = [_myMomentModel.messages objectAtIndex:indexPath.row];
+    if (!message.downloaded && message.localVideoPath) {
+        _downloadVideoView.alpha = 1;
+        [_downloadVideoView showWithAnimation];
+        [_downloadVideoView downloadVideoByMessage:message];
+    }else {
+        //TODO: Go to detail message
+        [self performSegueWithIdentifier:@"pushMomentDetailsView" sender:nil];
+//        [self performSegueWithIdentifier:@"momentDetailsTrending" sender:nil];
+    }
+}
+
+#pragma mark - Button New moment
+
+-(void)callPullDownRequest:(UIButton *)btn{
+    [self getAllMessage];
+}
+
+#pragma mark - DownloadVideo delegate
+- (void)downloadVideoSuccess:(MessageObject *)messageObject {
+    messageObject.downloaded = YES;
+    [_downloadVideoView hideWithAnimation];
+    //TODO: Go to detail message
+    [self performSegueWithIdentifier:@"pushMomentDetailsView" sender:nil];
+//    [self performSegueWithIdentifier:@"momentDetailsTrending" sender:nil];
+}
+
+- (void)downloadVideoFailure:(MessageObject *)messageObject  {
+    [_downloadVideoView hideWithAnimation];
+    [UIAlertView showTitle:@"Error" message:@"Cann't download this video"];
+}
+
+#pragma mark - Refresh control management
+
+- (void)refreshTableView {
+    [_myMomentModel getMyMessagesSuccess:^(id result) {
+        NSLog(@"Message found: %lu",(unsigned long)_myMomentModel.messages.count);
+        [self refreshTableViewDone];
+    } failure:^(NSError *error) {
+        [self refreshTableViewDone];
+    }];
+    _newsMomentButton.hidden = YES;
+}
+
+- (void)refreshTableViewDone {
+    [_refreshControl endRefreshing];
+    [_myMessagesTableView reloadData];
+}
+
+#pragma mark - FBConnectViewController Delegate
+
+-(void)fbConnectViewController:(FBConnectViewController *)vc didConnectFacebookSuccess:(id)response{
+    [self getAllMessage];
+}
 
 @end
